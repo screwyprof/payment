30dd978b19ec255f987a4490c0a4210a4f9a3420
Max O'Donnell <screwyprof@gmail.com>
2019-02-20 00:21:33 +0300

    Fix code coverage: do not exclude uncovered files

---
+++ tools/coverage.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#see https://github.com/ory/go-acc
+touch ./coverage.tmp
+echo 'mode: atomic' > coverage.txt
+go list ./... | grep -v /cmd | grep -v /vendor | xargs -n1 -I{} sh -c 'go test -race -covermode=atomic -coverprofile=coverage.tmp -coverpkg $(go list ./... | grep -v /vendor | tr "\n" ",") {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
